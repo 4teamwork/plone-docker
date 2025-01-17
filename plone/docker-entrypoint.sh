95346ae37abd692c231a118d81b2a0904486cea0
Thomas Buchberger <t.buchberger@4teamwork.ch>
2025-01-17 18:34:05 +0100

    Move entrypoint scripts into subdirectory

--- plone/docker-entrypoint.sh
+++ plone/docker-entrypoint.sh
@@ -5,7 +5,7 @@ ZOPE_RUN="/app/bin/runzope"
 ZOPE_CTL="/app/bin/zopectl"
 export INSTANCE_HOME
 
-python create_zope_conf.py "$CONFIG_FILE"
+python /app/entrypoint.d/create_zope_conf.py "$CONFIG_FILE"
 
 if [ $# -eq 0 ]
 then
--- zeoserver/docker-entrypoint.sh
+++ zeoserver/docker-entrypoint.sh
@@ -4,7 +4,7 @@ CONFIG_FILE="/app/etc/zeo.conf"
 ZEO_RUN="/app/bin/runzeo"
 export INSTANCE_HOME
 
-python create_zeo_conf.py "$CONFIG_FILE"
+python /app/entrypoint.d/create_zeo_conf.py "$CONFIG_FILE"
 
 [[ -d "/data/filestorage" ]] || mkdir /data/filestorage
 exec "$ZEO_RUN" -C "$CONFIG_FILE" "$@"
